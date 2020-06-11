4e2df5b603f40461f1bbee143ab112535b02a2f7
quangtuyendev <quangtuyennguyen09@gmail.com>
2020-06-12 06:34:59 +0700

    [master] edit package.json file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'deploy', 'index.html'));
 });
 app.listen(port);
